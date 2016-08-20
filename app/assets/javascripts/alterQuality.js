13624a655dc8962090e4c0ed330f635a844c449a
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-08-20 14:57:10 -0600

    Completed up and down buttons for new ideas

--- app/assets/javascripts/alterQuality.js
+++ app/assets/javascripts/alterQuality.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
-  upgradeQuality()
-  downgradeQuality()
+  upgradeQuality();
+  downgradeQuality();
   });
 
 function upgradeQuality(){
-  $('.any-idea').delegate('#upgrade-quality', 'click', function(){
-    var id = $(this).closest('#idea').data('idea-id')
-    var closestIdea = $(this).closest('#idea')
+  $(document).on('click', '.upgrade-quality', function(){
+    var id = $(this).closest('#idea').data('idea-id');
+    var closestIdea = $(this).closest('#idea');
   $.ajax({
     type: "PATCH",
     url: "/api/v1/ideas/" + id,
     data: {changeValue: "increase"},
     dataType: "json",
     success: function(data){
-    closestIdea.find('#idea-quality-show').text(data.quality)
+    closestIdea.find('#idea-quality-show').text(data.quality);
     }
-    })
-})
+  });
+});
 }
 
 function downgradeQuality(){
-  $('.any-idea').delegate('#downgrade-quality', 'click', function(){
-    var id = $(this).closest('#idea').data('idea-id')
-    var closestIdea = $(this).closest('#idea')
+  $(document).on('click', '.downgrade-quality', function(){
+    var id = $(this).closest('#idea').data('idea-id');
+    var closestIdea = $(this).closest('#idea');
   $.ajax({
     type: "PATCH",
     url: "/api/v1/ideas/" + id,
     data: {changeValue: "decrease"},
     dataType: "json",
     success: function(data){
-    closestIdea.find('#idea-quality-show').text(data.quality)
+    closestIdea.find('#idea-quality-show').text(data.quality);
     }
-    })
-})
+  });
+});
 }
--- app/assets/javascripts/createIdea.js
+++ app/assets/javascripts/createIdea.js
@@ -11,7 +11,7 @@ function createIdea(){
       url: "api/v1/ideas",
       data: ideaParams,
       success: function (idea){
-        var formatIdea = '<div class="any-idea"> <p> <div class="created-idea" data-idea-id = ' + idea.id + ' id="idea"> <ul> <strong>Title: </strong> <li id="idea-title-info" data-title-id = ' + idea.id + ' contentEditable="true">' + idea.title + '</li> <strong>Body: </strong><li id="idea-body-info" data-body-id = ' + idea.id + ' contentEditable="true">' + idea.body + '</li><strong>Quality: </strong> <li id="idea-quality-show">' + idea.quality + '</li> <button id="upgrade-quality" type="button"> Up </button> <button id="downgrade-quality" type="button"> Down </button> <div class="remove-button"> <input type="submit" value="Delete"> </div> </ul> </p> </div> </div>';
+        var formatIdea = '<div class="any-idea"> <p> <div class="created-idea" data-idea-id = ' + idea.id + ' id="idea"> <ul> <strong>Title: </strong> <li id="idea-title-info" data-title-id = ' + idea.id + ' contentEditable="true">' + idea.title + '</li> <strong>Body: </strong><li id="idea-body-info" data-body-id = ' + idea.id + ' contentEditable="true">' + idea.body + '</li><strong>Quality: </strong> <li id="idea-quality-show">' + idea.quality + '</li> <button class="upgrade-quality" type="button"> Up </button> <button class="downgrade-quality" type="button"> Down </button> <div class="remove-button"> <input type="submit" value="Delete"> </div> </ul> </p> </div> </div>';
         $('.all-ideas').prepend(formatIdea);
         $('#idea-title').val('');
         $('#idea-body').val('');
